a1210726055c2bf689d338a3f498e629ae5b6b31
Angad Isher <angadisher1@gmail.com>
2026-04-15 14:41:12 +0530

    Update Main.java

--- JAVA/CodingSET5/handleDivisionByZero/Main.java
+++ JAVA/CodingSET5/handleDivisionByZero/Main.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the Numenator and Denominator");
+         int nu = sc.nextInt();
+         int de = sc.nextInt();
+         try{
+             int result = nu/de;
+             System.out.println(result);
+         }
+         catch(ArithmeticException e){
+              System.out.println("Denominator can't be zero");
+         }
+        
+    }
+}
