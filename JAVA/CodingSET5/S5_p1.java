e306a3e897832d8f18d06631a0bdf4dc5eba9289
Angad Isher <angadisher1@gmail.com>
2026-04-19 16:07:59 +0530

    Update and rename JAVA/CodingSET5/handleDivisionByZero/Main.java to JAVA/CodingSET5/S5_p1.java

--- JAVA/CodingSET5/S5_p1.java
+++ JAVA/CodingSET5/S5_p1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class S5_p1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
          System.out.println("Enter the Numenator and Denominator");
